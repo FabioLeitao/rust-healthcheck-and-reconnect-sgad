19c0ca971d9fdbc52451134b06768c3aa5592173
Fabio Leitao <fabio.tleitao@gmail.com>
2024-01-25 17:04:19 -0300

    Versao com driver de instant client do OracleDB, validado no Windows

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate oracle;
+
+use oracle::Connection;
+
 fn main() {
-    println!("Hello, world!");
+    let conn = Connection::connect("tosp", "Athenas2018", "10.129.48.68:1521/tosprd").unwrap();
+    {
+        /*
+        let (version, banner) = conn.server_version().unwrap();
+        println!("Oracle Version: {}", version);
+        println!("--- Version Banner ---");
+        println!("{}", banner);
+        println!("---------------|---------------|---------------|");
+        */
+        let select_sql = "SELECT ID, KEY, VALUE, UPDATED FROM CORE.cosetting WHERE id = 681";
+        let rows = conn.query(select_sql, &[]).unwrap();
+        for r in rows {
+            let row = r.unwrap();
+            let id: String = row.get("ID").unwrap();
+            let key: String = row.get("KEY").unwrap();
+            let value: String = row.get("VALUE").unwrap();
+            let updated: String = row.get("UPDATED").unwrap();
+            if value == "TRUE" {
+                println!("{} {} {} {}", id, key, value, updated);
+            } else {
+                println!("{} {} {} {}", id, key, value, updated);
+                let update_sql = "UPDATE CORE.cosetting SET VALUE = 'TRUE' WHERE id = 681";
+                conn.execute(update_sql, &[]).unwrap();
+                conn.commit().unwrap();
+            }
+        }
+    }
 }
